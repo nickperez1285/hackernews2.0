6708c80babff006c3daa281578bdfbfb2b76399f
nickperez1285 <nickperez1285@gmail.com>
2020-09-13 23:43:27 -0700

    link 5

--- src/components/home.js
+++ src/components/home.js
@@ -61,10 +61,10 @@ const Home = (props) => {
                 }                {post.points} points by {post.author} | 
 
              
-                             <Link to={'/comment/' + post.objectID} style = {{
+                             <a href to={'/comment/' + post.objectID} style = {{
                 textDecoration: "none",
                 color: 'black',
-            }}> {post.num_comments} comments</Link>
+            }}> {post.num_comments} comments</a>
                 </td>
                 </tbody>
             
